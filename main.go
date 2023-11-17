29af0d1ac6b2ecf2ce1f2493a30c30b9d62e85ee
Roberto Santalla <roberto@santalla.io>
2023-11-17 13:02:42 +0100

    main: add main.go

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+
+	"github.com/grafana/go-depsync/deps"
+)
+
+func main() {
+	gomod := flag.String("gomod", "./go.mod", "Path to the local go.mod file")
+	parent := flag.String("parent", "", "Name of the parent package to sync dependencies with")
+	flag.Parse()
+
+	if *parent == "" {
+		flag.Usage()
+		log.Fatalf("You must specify the name of the parent package")
+	}
+
+	ownDeps, err := deps.FromGomodFile(*gomod)
+	if err != nil {
+		log.Fatalf("Couldn't parse own dependencies: %v", err)
+	}
+
+	parentVer, hasParent := ownDeps[*parent]
+	if !hasParent {
+		log.Fatalf("Parent package %q not found in local go.mod %q", *parent, *gomod)
+	}
+
+	log.Printf("Found parent %s@%s", *parent, parentVer)
+
+	parentDeps, err := deps.FromModule(*parent, parentVer)
+	if err != nil {
+		log.Fatalf("Cannot parse dependencies of %q: %v", *parent, err)
+	}
+
+	fmt.Println(deps.GoGetCommand(deps.Mismatched(ownDeps, parentDeps)))
+}
